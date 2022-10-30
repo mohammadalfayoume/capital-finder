7a1f2a00f92dfa4bb2ddcdfe6422eb32cd0c1d30
Mohammad Alfayoume <mabed3819@gmail.com>
2022-10-31 01:05:55 +0200

    bug : typo2

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -11,32 +11,31 @@ class handler(BaseHTTPRequestHandler):
 
     if 'country' not in dictionary and 'capital' not in dictionary:
       message="please, provide us with country or capital query!!"
-      
+
     if 'country' in dictionary:
       country=dictionary['country']
       url= f'https://restcountries.com/v3.1/name/{country}'
       req= requests.get(url)
       data=req.json()
-      if data["message"]=='Not Found':
-        message= "Invalid country name, try again"
-        self.send_response(404)
-      else:
-        capital=data[0]['capital'][0]
-        message= f'The capital of {country.capitalize()} is {capital}'
-        self.send_response(200)
+      # if data['message']=='Not Found':
+      #   message= "Invalid country name, try again"
+      #   self.send_response(404)
+      capital=data[0]['capital'][0]
+      print(capital)
+      message= f'The capital of {country.capitalize()} is {capital}'
+      self.send_response(200)
 
     if 'capital' in dictionary:
       capital=dictionary['capital']
       url= f'https://restcountries.com/v3.1/capital/{capital}'
       req= requests.get(url)
       data=req.json()
-      if data["message"]=='Not Found':
-        message= "Invalid capital name, try again"
-        self.send_response(404)
-      else:
-        country=data[0]['name']['common']
-        message= f'{capital.capitalize()} is the capital of {country}'
-        self.send_response(200)
+      # if data["message"]=='Not Found':
+      #   message= "Invalid capital name, try again"
+      #   self.send_response(404)
+      country=data[0]['name']['common']
+      message= f'{capital.capitalize()} is the capital of {country}'
+      self.send_response(200)
 
     
     self.send_header('Content-type', 'text/plain')
